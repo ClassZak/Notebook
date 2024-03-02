59cb13d939561dc31d23473bfa73a87224d9495c
Захар Голуб <pahargolub2@gmail.com>
2024-03-02 11:58:23 +0300

    Добавленно открытие файлов

--- Notebook/MainWindow.xaml.cs
+++ Notebook/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +16,38 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
+
 namespace Notebook
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+
+    
+    
+
     public partial class MainWindow : Window
     {
+        const string MAIN_TITLE = "Блокнот";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if ((bool)(openFileDialog.ShowDialog()))
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                this.PathField.Text= fileInfo.FullName;
+                this.Title = MAIN_TITLE + " - " + fileInfo.Name;
+                this.InputField.Text=File.ReadAllText(openFileDialog.FileName);
+            }
         }
     }
 }
