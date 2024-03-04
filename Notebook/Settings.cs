0705851c98e3d355180f9ba14b36ea088012fbf4
Захар Голуб <pahargolub2@gmail.com>
2024-03-04 21:54:33 +0300

    Начало разработки сохранения настроек

--- Notebook/Settings.cs
+++ Notebook/Settings.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using System.IO;
 
 namespace Notebook
 {
@@ -17,15 +18,38 @@ namespace Notebook
         public System.Windows.Media.Brush solidBackground;
 
         public bool IsImageBrush = false;
+        public bool IsStretch = false;
 
         public Settings()
         {
-            fontFamily = new System.Windows.Media.FontFamily("Consolas");
-            fontScale = 12;
-            foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0,0,0));
-            solidBackground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255,255,255));
+            if(!File.Exists("Settings.txt"))
+            {
+                fontFamily = new System.Windows.Media.FontFamily("Consolas");
+                fontScale = 12;
+                foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0,0,0));
+                solidBackground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255,255,255));
 
-            IsImageBrush = false;
+                IsImageBrush = false;
+                IsStretch = false;
+
+
+                FileStream fileStream = new FileStream("Settings.txt", FileMode.Create, FileAccess.Write);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+                streamWriter.WriteLine(fontFamily.ToString());
+                streamWriter.WriteLine(fontScale.ToString());
+                streamWriter.WriteLine(foreground.ToString());
+                streamWriter.WriteLine("null");
+                streamWriter.WriteLine(solidBackground.ToString());
+                streamWriter.WriteLine(IsImageBrush.ToString());
+                streamWriter.WriteLine(IsStretch.ToString());
+                streamWriter.Close();
+
+            }
+            else
+            {
+                FileStream fileStream = new FileStream("Settings.txt", FileMode.Open, FileAccess.Read);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+            }
         }
     }
 }
--- Notebook/SettingsWindow.xaml.cs
+++ Notebook/SettingsWindow.xaml.cs
@@ -23,31 +23,50 @@ namespace Notebook
     /// </summary>
     public partial class SettingsWindow : Window
     {
-        public static Settings settings = new Settings();
+        public static Settings settings;
 
         public SettingsWindow()
         {
             InitializeComponent();
             LoadColorsForComboBox();
             EnableSettings();
-            this.SizeTextBox.Text = this.TextSample.FontSize.ToString();
+            this.SizeTextBox.Text = this.Sketch.FontSize.ToString();
+            this.ImageRadio.IsChecked = settings.IsImageBrush;
         }
 
 
 
         private void EnableSettings()
         {
-            this.Sketch.Fill=settings.imageBackground;
-            this.TextSample.Foreground=settings.foreground;
-            this.TextSample.FontFamily=settings.fontFamily;
-            this.TextSample.FontSize=settings.fontScale;
-            this.TextSample.Background=settings.solidBackground;
+            
+            this.Sketch.Foreground=settings.foreground;
+            this.Sketch.FontFamily=settings.fontFamily;
+            this.Sketch.FontSize=settings.fontScale;
+            int i = 0;
+            foreach(var f in this.FontMenu.Items)
+            {
+                
+                string fstr = (f.ToString()).Substring(f.ToString().IndexOf(':')+2);
+                string setfstr = settings.fontFamily.ToString();
+                if(setfstr == fstr)
+                {
+                    this.FontMenu.SelectedIndex = i;
+                    break;
+                }
+                ++i;
+            }
 
             if(settings.IsImageBrush)
+            {
                 this.ImageRadio.IsChecked=true;
+                this.Sketch.Background=settings.imageBackground;
+            }
             else
+            {
                 this.SolidBrusnRadio.IsChecked = true;
-
+                this.Sketch.Background=settings.solidBackground;
+            }
+            this.PaveCheckBox.IsChecked = settings.IsStretch;
         }
 
 
@@ -69,30 +88,34 @@ namespace Notebook
         private void FontMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if(this.IsInitialized)
-            switch(this.FontMenu.SelectedIndex)
             {
-                case 0:
-                    this.TextSample.FontFamily = new System.Windows.Media.FontFamily("Consolas");
-                    break;
-                case 1:
-                    this.TextSample.FontFamily = new System.Windows.Media.FontFamily("Impact");
-                    break;
-                case 2:
-                    this.TextSample.FontFamily = new System.Windows.Media.FontFamily("Arial");
-                    break;
-                case 3:
-                    this.TextSample.FontFamily = new System.Windows.Media.FontFamily("Wingdings");
-                    break;
+                switch(this.FontMenu.SelectedIndex)
+                {
+                    case 0:
+                        this.Sketch.FontFamily = new System.Windows.Media.FontFamily("Consolas");
+                        break;
+                    case 1:
+                        this.Sketch.FontFamily = new System.Windows.Media.FontFamily("Impact");
+                        break;
+                    case 2:
+                        this.Sketch.FontFamily = new System.Windows.Media.FontFamily("Arial");
+                        break;
+                    case 3:
+                        this.Sketch.FontFamily = new System.Windows.Media.FontFamily("Wingdings");
+                        break;
+                }
+                settings.fontFamily = this.Sketch.FontFamily;
             }
+            
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if(this.IsInitialized)
             {
-                TextSample.FontSize = (uint)((Slider)(sender)).Value;
+                Sketch.FontSize = (uint)((Slider)(sender)).Value;
                 this.SizeTextBox.Text= ((uint)((Slider)(sender)).Value).ToString();
-                settings.fontScale = (uint)TextSample.FontSize;
+                settings.fontScale = (uint)Sketch.FontSize;
             }
         }
 
@@ -105,7 +128,7 @@ namespace Notebook
                     byte fontSize=Convert.ToByte(this.SizeTextBox.Text);
                     if (fontSize >= 2 && fontSize <= 96)
                     {
-                        this.TextSample.FontSize = fontSize;
+                        this.Sketch.FontSize = fontSize;
                         settings.fontScale=fontSize;
                     }
                         
@@ -119,58 +142,109 @@ namespace Notebook
         private void OpenImageButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            if((bool)openFileDialog.ShowDialog())
+
+            
+            bool succesSelected = false;
+            while (!succesSelected)
+            if ((bool)openFileDialog.ShowDialog())
             {
-                string FullName=openFileDialog.FileName;
-                if(FullName.EndsWith(".png") || FullName.EndsWith(".jpg") || FullName.EndsWith(".bmp"))
+                string FullName = openFileDialog.FileName;
+                if (FullName.EndsWith(".png") || FullName.EndsWith(".jpg") || FullName.EndsWith(".bmp"))
                 {
                     ImageBrush imageBrush = new ImageBrush();
                     BitmapImage image = new BitmapImage(new Uri(FullName));
-                    imageBrush.ImageSource = image;
-                    imageBrush.Viewport=new Rect(0,0,image.Width,image.Height);
-                    imageBrush.ViewportUnits = BrushMappingMode.Absolute;
+                    if ((bool)this.PaveCheckBox.IsChecked)
+                    {
+                        imageBrush.ImageSource = image;
+                        imageBrush.TileMode = TileMode.Tile;
+                        imageBrush.Viewport = new Rect(0, 0, image.Width, image.Height);
+                        imageBrush.ViewportUnits = BrushMappingMode.Absolute;
+                    }
+                    else
+                    {
+                        imageBrush.ImageSource = image;
+                    }
+                    this.Sketch.Background = imageBrush;
+                    settings.imageBackground = this.Sketch.Background;
 
-                    this.Sketch.Fill= imageBrush;
-                    this.PathInfo.Text=FullName;
+                    this.Sketch.Background = imageBrush;
+                    this.PathInfo.Text = FullName;
+                    succesSelected = true;
                 }
                 else
-                    MessageBox.Show("Неверный формат!","Ошибка!",MessageBoxButton.OK,MessageBoxImage.Error);
+                    MessageBox.Show("Неверный формат!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            else
+            {
+                this.SolidBrusnRadio.IsChecked = true;
+                succesSelected = true;
+                return;
+            }
+
+                
             PaveCheckBox_Checked(null, null);
         }
 
         private void PaveCheckBox_Checked(object sender, RoutedEventArgs e)
         {
+            if (!settings.IsImageBrush)
+                return;
+            //OpenImageButton_Click(OpenImageButton, null);
+            settings.IsImageBrush = true;
+            this.ImageRadio.IsChecked = true;
             try
             {
                 if (this.PathInfo.Text!=null)
                 {
-                    if((bool)this.PaveCheckBox.IsChecked)
+                    if(settings.imageBackground==null)
                     {
-                        ImageBrush imageBrush = new ImageBrush();
-                        BitmapImage image = new BitmapImage(new Uri(this.PathInfo.Text));
-                        imageBrush.ImageSource = image;
-                        imageBrush.TileMode = TileMode.Tile;
-                        imageBrush.Viewport = new Rect(0, 0, image.Width, image.Height);
-                        imageBrush.ViewportUnits = BrushMappingMode.Absolute;
+                        if((bool)this.PaveCheckBox.IsChecked)
+                        {
+                            ImageBrush imageBrush = new ImageBrush();
+                            BitmapImage image = new BitmapImage(new Uri(this.PathInfo.Text));
+                            imageBrush.ImageSource = image;
+                            imageBrush.TileMode = TileMode.Tile;
+                            imageBrush.Viewport = new Rect(0, 0, image.Width, image.Height);
+                            imageBrush.ViewportUnits = BrushMappingMode.Absolute;
 
-                        this.Sketch.Fill = imageBrush;
+                            this.Sketch.Background = imageBrush;
+                        }
+                        else
+                        {
+                            ImageBrush imageBrush = new ImageBrush();
+                            BitmapImage image = new BitmapImage(new Uri(PathInfo.Text));
+                            imageBrush.ImageSource = image;
+
+                            this.Sketch.Background = imageBrush;
+                        }
                     }
                     else
                     {
-                        ImageBrush imageBrush = new ImageBrush();
-                        BitmapImage image = new BitmapImage(new Uri(PathInfo.Text));
-                        imageBrush.ImageSource = image;
+                        if ((bool)this.PaveCheckBox.IsChecked)
+                        {
+                            ImageBrush imageBrush = (ImageBrush)settings.imageBackground;
+                            imageBrush.TileMode=TileMode.Tile;
+                            imageBrush.Viewport=new Rect(0,0,imageBrush.ImageSource.Width,imageBrush.ImageSource.Height);
+                            imageBrush.ViewportUnits= BrushMappingMode.Absolute;
 
-                        this.Sketch.Fill = imageBrush;
+                            this.Sketch.Background = imageBrush;
+                        }
+                        else
+                        {
+                            ImageBrush imageBrush = new ImageBrush();
+                            imageBrush.ImageSource = ((ImageBrush)(settings.imageBackground)).ImageSource;
+                            
+                            this.Sketch.Background = imageBrush;
+                        }
                     }
-                    settings.imageBackground = this.Sketch.Fill;
+                    settings.IsStretch = (bool)this.PaveCheckBox.IsChecked;
+                    settings.imageBackground = this.Sketch.Background;
                 }
-                
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message,"Ошибка!",MessageBoxButton.OK, MessageBoxImage.Error);
+                OpenImageButton_Click(OpenImageButton, null);
             }
         }
 
@@ -179,30 +253,46 @@ namespace Notebook
         {
             string colorString = ((ComboBox)(sender)).SelectedValue.ToString();
             var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(colorString);
-            this.TextSample.Foreground = new SolidColorBrush(color);
+            this.Sketch.Foreground= new SolidColorBrush(color);
             if(settings!=null)
             settings.foreground= new SolidColorBrush(color);
         }
         private void BackgroundColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string colorString = ((ComboBox)(sender)).SelectedValue.ToString();
+            settings.IsImageBrush = false;
             var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(colorString);
-            this.TextSample.Background = new SolidColorBrush(color);
+            SolidColorBrush solidColorBrush = new SolidColorBrush(color);
             if (settings != null)
-                settings.solidBackground = new SolidColorBrush(color);
+                settings.solidBackground = solidColorBrush;
+
+
+            this.SolidBrusnRadio.IsChecked = true;
+            this.Sketch.Background = solidColorBrush;
         }
 
 
 
         private void RadioButtonImageBrush_Checked(object sender, RoutedEventArgs e)
         {
-            if (settings != null)
+            if (this.IsInitialized)
+            {
                 settings.IsImageBrush = true;
+                if (settings.imageBackground == null)
+                    OpenImageButton_Click(null, null);
+                else
+                    this.Sketch.Background= settings.imageBackground;
+            }
         }
         private void RadioButtonSolidBrush_Checked(object sender, RoutedEventArgs e)
         {
-            if(settings!=null)
+            if(this.IsInitialized)
+            {
                 settings.IsImageBrush = false;
+                if (this.Sketch != null)
+                this.Sketch.Background=settings.solidBackground;
+            }
+                
         }
 
 
